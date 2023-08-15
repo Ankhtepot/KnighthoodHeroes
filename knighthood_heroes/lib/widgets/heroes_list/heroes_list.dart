93faa79c9332d5092d4ec193e6b6bba7f6b38fdd
ZavodnyPetr <petr.zavodny@sensearena.com>
2023-08-15 08:16:17 +0200

    not working solution

--- knighthood_heroes/lib/widgets/heroes_list/heroes_list.dart
+++ knighthood_heroes/lib/widgets/heroes_list/heroes_list.dart
@@ -3,40 +3,49 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:knighthood_heroes/general/text_rounded_with_background.dart';
 import 'package:knighthood_heroes/models/knighthood_hero.dart';
 import 'package:knighthood_heroes/providers/hero_filters_provider.dart';
+import 'package:knighthood_heroes/providers/heroes_expanded_states.dart';
 import 'package:knighthood_heroes/widgets/heroes_list/hero_card.dart';
 import 'package:knighthood_heroes/widgets/heroes_list/hero_card_detailed.dart';
 
-class HeroesList extends ConsumerStatefulWidget {
+// var heroesExpandedStatesProvider = StateProvider<List<bool>>((ref) => []);
+
+class HeroesList extends ConsumerWidget {
   const HeroesList({super.key});
 
+//   @override
+//   ConsumerState<HeroesList> createState() => _HeroesListState();
+// }
+
+// class _HeroesListState extends ConsumerState<HeroesList> {
+//   List<bool> expandedStates = [];
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   heroesExpandedStatesProvider.i;
+  // }
+
+  // @override
+  // void didUpdateWidget(covariant HeroesList oldWidget) {
+  //   super.didUpdateWidget(oldWidget);
+  //   expandedStates = List.filled(widget.heroes.length, false);
+  // }
+
+  // void toggleExpansion(int index) {
+  //   setState(() {
+  //     expandedStates[index] = !expandedStates[index];
+  //   });
+  // }
+
   @override
-  ConsumerState<HeroesList> createState() => _HeroesListState();
-}
-
-class _HeroesListState extends ConsumerState<HeroesList> {
-  List<bool> expandedStates = [];
-
-  @override
-  void initState() {
-    super.initState();
-    expandedStates = List.filled(widget.heroes.length, false);
-  }
-
-  @override
-  void didUpdateWidget(covariant HeroesList oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    expandedStates = List.filled(widget.heroes.length, false);
-  }
-
-  void toggleExpansion(int index) {
-    setState(() {
-      expandedStates[index] = !expandedStates[index];
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     List<KnighthoodHero> filteredHeroes = ref.watch(filteredHeroesProvider).exclusive;
+    final expandedStates = ref.watch(heroesExpandedStatesProvider);
+    final expandedStatesNotifier = ref.read(heroesExpandedStatesProvider.notifier);
+
+    if (expandedStates.isEmpty) {
+      expandedStatesNotifier.initState(filteredHeroes.length);
+    }
 
     if (filteredHeroes.isNotEmpty) {
       return ListView.builder(
@@ -45,7 +54,7 @@ class _HeroesListState extends ConsumerState<HeroesList> {
           final isExpanded = expandedStates[index];
           return InkWell(
             onTap: () {
-              toggleExpansion(index);
+              expandedStates[index] = !isExpanded;
             },
             child: isExpanded ? HeroCardDetailed(filteredHeroes[index]) : HeroCard(filteredHeroes[index]),
           );
